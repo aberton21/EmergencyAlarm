941c07177cbb6f0f8a17bd6c8eddae99292811bd
aberton21 <87664075+aberton21@users.noreply.github.com>
2023-02-13 14:07:16 -0800

    Update Audio.py

--- Emergency_Audio/Audio.py
+++ Emergency_Audio/Audio.py
@@ -4,37 +4,68 @@ from ctypes import cast, POINTER
 import serial
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from subprocess import PIPE
 import psutil
+import os
 import subprocess
 import time
 
+
 arduinoSerialData = serial.Serial('com3', 9600)
-time.sleep(1)
-isOpen = "chrome.exe" in (i.name() for i in psutil.process_iter())
+
+audio = None
+
+audio_file = "emergency-alarm-with-reverb-29431.mp3"
+
+def switch_on():
+    global audio
+    print("Alarm activated")
+
+    # Get default audio device using pycaw
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+    # Get current volume
+    currentVolumeDb = volume.GetMasterVolumeLevel()
+    volume.SetMasterVolumeLevel(-48.0, None)
+
+    # Kill all running applications
+    os.system("pkill -f")
+    # subprocess.Popen("TASKKILL /F /IM chrome.exe 2> nul", shell=True)
+
+    # Play audio
+    audio = subprocess.Popen(["wmplayer.exe", audio_file])
+
+def switch_off():
+    print("Alarm deactivated")
 
 while True:
+    myData = ''
     if (arduinoSerialData.inWaiting()>0):
         myData = arduinoSerialData.readline()
         myData = str(myData,'utf-8')
         myData = myData.strip('\r\n')
         print(myData)
         
-        if myData == 'Switch: ON':
-            if ("chrome.exe" in (i.name() for i in psutil.process_iter())):
-                subprocess.call("TASKKILL /F /IM chrome.exe 2> nul", shell=True)
+    if myData == 'Switch: ON':
 
-            # Get default audio device using pycaw
-            devices = AudioUtilities.GetSpeakers()
-            interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-            volume = cast(interface, POINTER(IAudioEndpointVolume))
+        isOpen = "chrome.exe" in (i.name() for i in psutil.process_iter())
+        if (isOpen):
+            subprocess.Popen("TASKKILL /F /IM chrome.exe 2> nul", shell=True)
+                
+                
+        # Get default audio device using pycaw
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-            # Get current volume
-            currentVolumeDb = volume.GetMasterVolumeLevel()
-            volume.SetMasterVolumeLevel(-25.0, None)
-
-            i = 0
-            while i < 113:
-                playsound('emergency-alarm-with-reverb-29431.mp3')
-                i += 1
+        # Get current volume
+        currentVolumeDb = volume.GetMasterVolumeLevel()
+        volume.SetMasterVolumeLevel(-48.0, None)
             
-            quit()
+        i = 0
+        while i < 57:
+            playsound('emergency-alarm-with-reverb-29431.mp3')
+            i += 1
+        break
