538c503b5f1dcb16ab4d15f379b81abe87512131
aberton21 <87664075+aberton21@users.noreply.github.com>
2023-09-18 16:03:08 -0700

    Update sketch.ino

--- Emergency_Audio/sketch.ino
+++ Emergency_Audio/sketch.ino
@@ -2,24 +2,22 @@
 
 ezButton toggleSwitch(7);
 
-void setup()
-{
-	Serial.begin(9600);
-	toggleSwitch.setDebounceTime(50);
+void setup() {
+  Serial.begin(9600);
+  toggleSwitch.setDebounceTime(50);
 }
-void loop()
-{
-	toggleSwitch.loop();
-	
-	if (toggleSwitch.isPressed())
-		Serial.println("Switch: OFF -> ON");
-	
-	if(toggleSwitch.isReleased())
-		Serial.println("Switch: ON -> OFF");
-	
-	int state = toggleSwitch.getState();
-	if (state == HIGH)
-		Serial.println("Switch: OFF");
-	else
-		Serial.println("Switch: ON");
+void loop() {
+  toggleSwitch.loop();
+
+  if (toggleSwitch.isPressed())
+    Serial.println("Switch: OFF -> ON");
+
+  if (toggleSwitch.isReleased())
+    Serial.println("Switch: ON -> OFF");
+
+  int state = toggleSwitch.getState();
+  if (state == HIGH)
+    Serial.println("Switch: OFF");
+  else
+    Serial.println("Switch: ON");
 }
